04af3ba27113c09b628f63a0fac008c0e8741d08
lummith <104735371+lummith@users.noreply.github.com>
2022-07-04 23:39:26 +0100

    Add files via upload

---
+++ DataObjects/Schedule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataObjects
+{
+    public record Schedule(int trayNumber)
+    {
+        public List<LightingInstruction> LightingInstructions { get; set; } 
+        public List<wateringInstruction> WateringInstructions { get; set; }
+    }
+
+    public record LightingInstruction(LightIntensity intensity, DateTime wateringTime);
+
+    public record wateringInstruction(short amount, DateTime wateringTime);
+    
+}
---
+++ DataObjects/input.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace DataObjects
+{
+    public class RecipeInput
+    {
+        public Input? input { get; set; }
+
+        public Input JsonToRecipes(string json) => JsonConvert.DeserializeObject<Input>(json);
+
+    }
+
+    public record Input(List<Tray> input);
+
+    public record Tray(int trayNumber, string recipeName, string startDate);
+
+}
