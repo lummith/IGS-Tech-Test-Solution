f1be26ccad653a2657a1cbf6990046393c0e9400
lummith <104735371+lummith@users.noreply.github.com>
2022-07-07 10:14:20 +0100

    Add files via upload

---
+++ QueryApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using DataObjects;
+using QueryApp;
+using QueryProcessor;
+
+try
+{ 
+    // get input
+    string fileInput = string.Empty;
+    if (File.Exists(args[0]))
+        fileInput = File.ReadAllText(args[0]);
+    else
+    {
+        Console.WriteLine("Input file does not exist in the location specified.");
+        Console.ReadKey(true);
+        System.Environment.Exit(0);
+    }
+
+    // send query to api
+    SendAgent sa = new SendAgent();
+    var instructions = await sa.sendAndReceive();
+
+    if (string.IsNullOrEmpty(instructions)) 
+    {
+        Console.ReadKey(true);
+        System.Environment.Exit(0);
+    }
+        
+
+    string errorMessage = string.Empty;
+    Processor qp = new Processor();
+    var schedule = qp.ProcessQuery(fileInput, instructions, out errorMessage);
+
+    qp.outputSchedul(schedule);
+
+}
+catch (TaskCanceledException tce)
+{
+    Console.WriteLine($"Exception occured while attempting: {tce.Message}");
+}
+catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+ 
+// build message
+// send message 
+// config file
+// process response
---
+++ QueryApp/SendAgent.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryApp
+{
+    public class SendAgent
+    {
+        private string url = "";
+
+        public SendAgent()
+        {
+            url = retriveUrl();
+        }
+
+        public async Task<string> sendAndReceive()
+        {           
+            var resString = string.Empty;
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+                    var response = await client.GetAsync(url);
+                    resString = await response.Content.ReadAsStringAsync();
+                }
+
+                return resString;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"An execption occured while attempting to send a request to the api: {e.Message}");
+                return string.Empty;
+            }
+
+
+        }
+
+        private string retriveUrl()
+        {
+            string urlFile = @"Files\URL.txt";
+            if (File.Exists(urlFile))
+            {
+                var temp = File.ReadAllText(urlFile);
+                if (!string.IsNullOrEmpty(temp))
+                    return temp;
+                else                
+                    return Resources.URL;                  
+            }                
+            else
+                return Resources.URL;
+        }
+    }
+}
